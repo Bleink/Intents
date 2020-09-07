04f710e0e488cde04e2f9e42f4b38a71b94b1c6c
Bleink <57928442+Bleink@users.noreply.github.com>
2020-09-06 23:31:57 -0500

    Necessary updates

---
+++ app/src/main/java/com/example/walmart/shoppingActivity.kt
@@ -0,0 +1,32 @@
+package com.example.walmart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.walmart.R
+import kotlinx.android.synthetic.main.activity_shopping.*
+
+class shoppingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+        var intent= getIntent()
+       var  emails= intent.getStringExtra("email")
+        welcome.setText("welcome $emails")
+    }
+
+    fun electronicClick(view: View) {
+        Toast.makeText(this, "You have chosen the Electronics category of shopping", Toast.LENGTH_LONG).show()
+    }
+    fun clothClick(view: View) {
+        Toast.makeText(this, "You have chosen the Clothing category of shopping", Toast.LENGTH_LONG).show()
+    }
+    fun beautyClick(view: View) {
+        Toast.makeText(this, "You have chosen the Beauty category of shopping", Toast.LENGTH_LONG).show()
+    }
+    fun foodClick(view: View) {
+        Toast.makeText(this, "You have chosen the Food category of shopping", Toast.LENGTH_LONG).show()
+    }
+}
